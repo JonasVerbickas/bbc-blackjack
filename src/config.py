d5a260d4a77e729bbfd4b8de10860a39cd5f334f
JonasVerbickas <jonasverbic@gmail.com>
2023-01-07 22:57:46 +0000

    New README.md

--- src/config.py
+++ src/config.py
@@ -1,3 +1,5 @@
+# ===================================================================================
+# HYPERPARAMETERS
 """
 Hyperparameters to configure the game and possibly use custom rules.
 Values for cards can be changed in Card.get_value() method or in Rank enum.
@@ -8,9 +10,14 @@ NUMBER_OF_ROUNDS = 5
 BLACKJACK_MULTIPLIER = 1.5
 MIN_BET = 10  # please keep it positive
 
-# these require changes in the code
-# or might cause issues with the test suite
-# so please don't change them!
-# I could implement them, but the exam period is coming up and I need to revise :)
+
+# ===================================================================================
+# CONSTANTS
+"""
+WARNING: changing these values might break the game
+or might cause issues with the test suite
+so please don't change them!
+I could implement them, but the exam period is coming up and I need to revise :)
+"""
 INITIAL_HAND_SIZE = 2
 BLACKJACK = 21
