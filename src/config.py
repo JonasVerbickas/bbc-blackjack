2ee00684d57f51163565eb818242930e659483ff
JonasVerbickas <jonasverbic@gmail.com>
2023-01-07 22:32:50 +0000

    Make config more clear

--- src/config.py
+++ src/config.py
@@ -3,9 +3,14 @@ Hyperparameters to configure the game and possibly use custom rules.
 Values for cards can be changed in Card.get_value() method or in Rank enum.
 """
 DEALER_STAND_THRESHOLD = 17
-BLACKJACK = 21
 INITIAL_BALANCE = 100
 NUMBER_OF_ROUNDS = 5
 BLACKJACK_MULTIPLIER = 1.5
-MIN_BET = 10 # please keep it positive
+MIN_BET = 10  # please keep it positive
+
+# these require changes in the code
+# or might cause issues with the test suite
+# so please don't change them!
+# I could implement them, but the exam period is coming up and I need to revise :)
 INITIAL_HAND_SIZE = 2
+BLACKJACK = 21
